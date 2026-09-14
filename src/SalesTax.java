8c0ab20011060e34bc7413ec821ec01e1a353f26
Hernandez-Ocampo <herna2jj@mail.uc.edu>
2026-09-14 11:53:09 -0400

    SalesTax

---
+++ src/SalesTax.java
@@ -0,0 +1,9 @@
+void main() {
+    double itemPrice = 8;
+    final double TAX_RATE = 0.05;
+    double salesTax = itemPrice * TAX_RATE;
+
+    System.out.println("the sales tax for " + itemPrice + (" is ") + salesTax);
+
+
+}
